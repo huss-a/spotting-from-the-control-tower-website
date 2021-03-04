530c9165f6a648de8cfa48d9a5ba5b99b87a126e
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-03-04 21:38:27 +0300

    ✨Mobile friendliness✨, burger menu for smaller screens

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,29 @@ tl.to('.text', {y: '0%', duration: 1, stagger: 0.2, delay: 0.4});
 tl.to('.slider', {x: '-100%', duration: 1.4, delay: 0.4});
 
 tl.to('.intro', {y: '-100%', duration: 1}, '-=1')
+
+const dom = document
+
+// Toggle nav
+const navSlide = () => {
+    const burger = dom.querySelector('.burger');
+    const nav = dom.querySelector('.nav-links');
+    const navLi = dom.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('burger-nav-active');
+        // animate links
+        navLi.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+            }
+            else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            };
+        });
+    });
+};
+
+
+
+
+navSlide();
